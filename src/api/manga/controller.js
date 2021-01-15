ef5247d56218aebf8cd60e571b58e3cec844e326
AdamGoodApp <raisadamo@gmail.com>
2021-01-15 17:50:02 +0900

    Add mangakakalot chapters api

--- src/api/manga/controller.js
+++ src/api/manga/controller.js
@@ -1,15 +1,11 @@
-const { Mangadex } = require('mangadex-api')
+import { getMangakakalotChapters } from '../../services/scraper/index'
 
 export const index = ({ querymen: { query, select, cursor } }, res, next) =>
   res.status(200).json([{ test: 'comon' }])
 
 export const show = async ({ params }, res, next) => {
   const { id } = params
-  const client = new Mangadex()
+  const searchResult = await getMangakakalotChapters(id)
 
-  await client.agent.login('uberpoopoo', 'rarro5-zocmuZ-zizkiw', false)
-
-  const result = await client.search({ title: id, with_hentai: true })
-
-  return res.status(200).json({ manga: result })
+  return res.status(200).json({ chapters: searchResult })
 }
---
+++ src/services/scraper/index.js
@@ -0,0 +1,23 @@
+const Xray = require('x-ray')
+const xray = Xray()
+
+const search = async (query) => {
+  const cleaned = query.replace(/ /g, '_')
+  return await xray(`https://mangakakalot.com/search/story/${cleaned}`, '.story_name a@href')
+}
+
+// Takes a searched title href and returns arrays of available chapters
+const getChapters = async (titleUrl) => {
+  const { titles, hrefs } = await xray(titleUrl, '.row-content-chapter', { titles: ['a @title'], hrefs: ['a @href'] })
+
+  return titles.map((title, index) => {
+    return { title: title, href: hrefs[index] }
+  })
+}
+
+export const getMangakakalotChapters = async (title) => {
+  const manga = await search(title)
+  const chapters = await getChapters(manga)
+
+  return chapters
+}
